5035baafc554f0f85b20c842686bba35c35abdee
kisuke <dostonlv@icloud.com>
2025-04-06 14:37:12 +0500

    Add UzNumber class for Uzbek number conversion and update exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { NumberToWord } from "./number";
+import { NumberToWord, UzNumber } from "./number";
 import { Latin, Cyrillic } from './words';
 import {latinToCyrillicConverter, cyrillicToLatinConverter} from "./converter"
 
 export function toWord(number: number) {
     return new NumberToWord().toWord(number);
+}
+
+export function toUzWord(number: number) {
+    return new UzNumber().toWord(number);
 }
 
 export function toLatin(text: string) {
