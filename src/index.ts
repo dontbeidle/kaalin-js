f487123ec7cb440ee48ca6c0eaeff6f8bb7b67fe
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2025-04-07 12:56:47 +0500

    Bump version to 1.9.1 and import converter module in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NumberToWord } from "./number";
 import { Latin, Cyrillic } from './words';
+import * as converter from './converter';
 
 export function toWord(number: number) {
     return new NumberToWord().toWord(number);
